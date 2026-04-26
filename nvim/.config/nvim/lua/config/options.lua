46677e16bf205f487c6140521adcffccfe676199
thanhdat77 <tdat772001@gmail.com>
2026-04-26 22:51:51 +0700

    add transparent neovim

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Function to clear backgrounds
+local function transparent_back()
+  local groups = { "Normal", "NormalNC", "SignColumn", "NormalFloat", "StatusLine" }
+  for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none", ctermbg = "none" })
+  end
+end
+
+-- Run it now and whenever you change a colorscheme
+transparent_back()
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = transparent_back,
+})
